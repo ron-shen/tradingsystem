8f9e9dffe7e861daacc38e477632384630bc1165
ronshen0404 <ronshen0404@gmail.com>
2022-07-26 12:47:49 +0800

    twelve api data handler

---
+++ tradingsystem/Data_Handler/twevel_real_time.py
@@ -0,0 +1,44 @@
+from Data_Handler.base import AbstractDataHandler
+import queue
+import time
+import requests
+
+
+class TwelveRealTimeBarHandler(AbstractDataHandler):
+    """
+    Get real time bar data using twelve data api
+    https://rapidapi.com/twelvedata/api/twelve-data1/
+    """
+    def __init__(self, timeframe, event_queue, 
+                 db_client=None
+        ):
+        self.db_client = db_client
+
+                
+    def get_next(self):
+        pass
+    
+    
+with open("/home/ron/Desktop/apikey", "r") as f:
+    api_key = f.read()[:-1]
+  
+
+url = "https://twelve-data1.p.rapidapi.com/time_series"
+
+querystring = {"symbol":"AMZN","interval":"1day","outputsize":"30","format":"json"}
+
+headers = {
+	"X-RapidAPI-Key": api_key,
+	"X-RapidAPI-Host": "twelve-data1.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+    
+    
+    
+
+
+                                   
+      
